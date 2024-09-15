61078d36e3c4d242418f6d9e2ddab05618dce0df
Darkmift <darkmift@gmail.com>
2024-09-15 15:57:54 +0300

    Add .gitignore and styles.css files

---
+++ MVC1/backend/src/server.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+let broadcaster:any;
+const port = 3000;
+
+app.use(express.static("public"));
+
+io.sockets.on("error", e => console.log(e));
+io.sockets.on("connection", socket => {
+  socket.on("broadcaster", () => {
+    broadcaster = socket.id;
+    socket.broadcast.emit("broadcaster");
+  });
+  socket.on("watcher", () => {
+    socket.to(broadcaster).emit("watcher", socket.id);
+  });
+  socket.on("offer", (id, message) => {
+    socket.to(id).emit("offer", socket.id, message);
+  });
+  socket.on("answer", (id, message) => {
+    socket.to(id).emit("answer", socket.id, message);
+  });
+  socket.on("candidate", (id, message) => {
+    socket.to(id).emit("candidate", socket.id, message);
+  });
+  socket.on("disconnect", () => {
+    socket.to(broadcaster).emit("disconnectPeer", socket.id);
+  });
+});
+server.listen(port, () => console.log(`Server is running on port ${port}`));
